16119e22c5c3b05a6512a9650bc72bc47c06afe3
KinGmapua <29995733+Vpaproject@users.noreply.github.com>
2021-12-22 16:04:25 +0800

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -1,26 +1,29 @@
-OvpnDownload_Port='80'
+#!/bin/bash
+# OpenVPN Ports
+OpenVPN_Port1='1103'
+OpenVPN_Port2='25222'
+Ohp_Port='7752'
+Squid_Port1='8000'
+Squid_Port2='3128'
+Squid_Port3='60000'
+OvpnDownload_Port='85'
+MyVPS_Time='Asia/Kuala_Lumpur'
+#############################
+ apt-get update
+ apt-get upgrade -y
+ apt-get install nginx 
 
-apt-get update
-apt-get upgrade -y
-apt-get install nginx
-
-cat <<'EOFnginx' > /etc/nginx/conf.d/bonveio-ovpn-config.conf
+ # Creating nginx config for our ovpn config downloads webserver
+ cat <<'myNginxC' > /etc/nginx/conf.d/bonveio-ovpn-config.conf
+# My OpenVPN Config Download Directory
 server {
- listen 0.0.0.0:85;
+ listen 0.0.0.0:myNginx;
  server_name localhost;
  root /var/www/openvpn;
  index index.html;
 }
-EOFnginx
+myNginxC
 
-rm -rf /etc/nginx/sites-*
-rm -rf /usr/share/nginx/html
-rm -rf /var/www/openvpn
-mkdir -p /var/www/openvpn
-wget https://raw.githubusercontent.com/GakodArmy/teli/main/index.html
-cp index.html /var/www/openvpn
-
-echo -e "[\e[32mInfo\e[0m] Creating OpenVPN client configs.."
  # Setting our nginx config port for .ovpn download site
  sed -i "s|myNginx|$OvpnDownload_Port|g" /etc/nginx/conf.d/bonveio-ovpn-config.conf
 
@@ -30,10 +33,12 @@ echo -e "[\e[32mInfo\e[0m] Creating OpenVPN client configs.."
  # Creating our root directory for all of our .ovpn configs
  rm -rf /var/www/openvpn
  mkdir -p /var/www/openvpn
- rm /var/www/openvpn/index.html
+ # rm /var/www/openvpn/index.html
 
- # Restarting nginx service
- systemctl restart nginx
+wget https://raw.githubusercontent.com/89870must73/DEB/main/index.html
+cp index.html /var/www/openvpn
+
+# finishing
 cd
-
-
+chown -R www-data:www-data /var/www/openvpn
+/etc/init.d/nginx restart
